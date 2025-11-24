f64b3adf101af5179ab18c26c4693d9d9a85cee1
Tio Paulinho Gameplays <elitebrotherbrasil@gmail.com>
2025-11-24 12:24:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3215266765-e23ca",
+  "appId": "1:971840042646:web:4b824d9daa26b5c406f4b7",
+  "apiKey": "AIzaSyCSX__BPlM23fYUUu-klcSc1HKLmNzlOQs",
+  "authDomain": "studio-3215266765-e23ca.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "971840042646"
+};
