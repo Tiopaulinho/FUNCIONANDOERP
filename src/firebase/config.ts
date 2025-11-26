0b0bb5eaa757723b0eabc2a7686897e59bf05248
prschmiedel <prschmiedel@gmail.com>
2025-11-26 02:55:46 +0000

    fix: Export firebaseConfig para corrigir erro de build

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,16 +1,13 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For more information on how to access Firebase in your app, visit:
 // https://firebase.google.com/docs/web/setup#access-firebase
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -22,7 +19,7 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-let firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 const auth = getAuth(firebaseApp);
 
