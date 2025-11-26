908ce5a1628e52925d829639ea1eaf728add7a48
prschmiedel <prschmiedel@gmail.com>
2025-11-26 03:57:24 +0000

    fix: Restaurar credenciais corretas do Firebase na branch main

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -8,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#access-firebase
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCHQp-0HF4O2NVv9aC2FGeKhOsS2lbgqnE",
+  authDomain: "erpnovo-483a1.firebaseapp.com",
+  projectId: "erpnovo-483a1",
+  storageBucket: "erpnovo-483a1.firebasestorage.app",
+  messagingSenderId: "280722278498",
+  appId: "1:280722278498:web:36637e9d80eed537d2293b",
+  measurementId: "G-16SGNW39FZ"
 };
 
 
